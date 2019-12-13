14edbcb21dbaf052c75b67afce308c2a49303bf0
Robert Hahn <877263+Maschina@users.noreply.github.com>
2019-12-13 15:07:20 +0100

    Tuples to observe both event and element

--- ObservableArray/ObservableArray.swift
+++ ObservableArray/ObservableArray.swift
@@ -9,10 +9,30 @@
 import Foundation
 import RxSwift
 
+public struct ArrayChangeTuple<Element> {
+    public let insertedIndices: [(index: Int, element: Element)]
+    public let deletedIndices: [(index: Int, element: Element)]
+    public let updatedIndices: [(index: Int, element: Element)]
+    
+    var hasInserts: Bool { return insertedIndices.count > 0 }
+    var hasUpdates: Bool { return updatedIndices.count > 0 }
+    var hasDeletes: Bool { return deletedIndices.count > 0 }
+
+    fileprivate init(inserted: [(index: Int, element: Element)] = [], deleted: [(index: Int, element: Element)] = [], updated: [(index: Int, element: Element)] = []) {
+        insertedIndices = inserted
+        deletedIndices = deleted
+        updatedIndices = updated
+    }
+}
+
 public struct ArrayChangeEvent {
     public let insertedIndices: [Int]
     public let deletedIndices: [Int]
     public let updatedIndices: [Int]
+    
+    var hasInserts: Bool { return insertedIndices.count > 0 }
+    var hasUpdates: Bool { return updatedIndices.count > 0 }
+    var hasDeletes: Bool { return deletedIndices.count > 0 }
 
     fileprivate init(inserted: [Int] = [], deleted: [Int] = [], updated: [Int] = []) {
         insertedIndices = inserted
@@ -23,9 +43,11 @@ public struct ArrayChangeEvent {
 
 public struct ObservableArray<Element>: ExpressibleByArrayLiteral {
     public typealias EventType = ArrayChangeEvent
+    public typealias EventTuple = ArrayChangeTuple
 
     internal var eventSubject: BehaviorSubject<EventType>!
     internal var elementsSubject: BehaviorSubject<[Element]>!
+    internal var tuplesSubject: BehaviorSubject<EventTuple<Element>>!
     internal var elements: [Element]
 
     public init() {
@@ -56,17 +78,32 @@ extension ObservableArray {
     public mutating func rx_events() -> Observable<EventType> {
         if eventSubject == nil {
             if elements.count > 0 {
-                eventSubject = BehaviorSubject<EventType>(value: ArrayChangeEvent(inserted: Array(0...elements.count-1), deleted: [], updated: []))
+                eventSubject = BehaviorSubject<EventType>(value: ArrayChangeEvent(inserted: Array(0..<elements.count), deleted: [], updated: []))
             } else {
                 eventSubject = BehaviorSubject<EventType>(value: ArrayChangeEvent(inserted: [], deleted: [], updated: []))
             }
         }
         return eventSubject
     }
+    
+    public mutating func rx_tuples() -> Observable<EventTuple<Element>> {
+        if tuplesSubject == nil {
+            if elements.count > 0 {
+                tuplesSubject = BehaviorSubject<EventTuple>(value: ArrayChangeTuple(inserted: elements.enumerated().map({ ($0, $1) }), deleted: [], updated: []))
+            } else {
+                tuplesSubject = BehaviorSubject<EventTuple>(value: ArrayChangeTuple(inserted: [], deleted: [], updated: []))
+            }
+        }
+        return tuplesSubject
+    }
 
     fileprivate func arrayDidChange(_ event: EventType) {
         elementsSubject?.onNext(elements)
         eventSubject?.onNext(event)
+    }
+    
+    fileprivate func arrayDidChange(_ tuples: EventTuple<Element>) {
+        tuplesSubject?.onNext(tuples)
     }
 }
 
@@ -106,6 +143,7 @@ extension ObservableArray: MutableCollection {
     public mutating func append(_ newElement: Element) {
         elements.append(newElement)
         arrayDidChange(ArrayChangeEvent(inserted: [elements.count - 1]))
+        arrayDidChange(ArrayChangeTuple(inserted: [(elements.count - 1, newElement)]))
     }
 
     public mutating func append<S : Sequence>(contentsOf newElements: S) where S.Iterator.Element == Element {
@@ -115,6 +153,7 @@ extension ObservableArray: MutableCollection {
             return
         }
         arrayDidChange(ArrayChangeEvent(inserted: Array(end..<elements.count)))
+        arrayDidChange(ArrayChangeTuple(inserted: newElements.enumerated().map({ ($0, $1) })))
     }
 
     public mutating func appendContentsOf<C : Collection>(_ newElements: C) where C.Iterator.Element == Element {
@@ -124,22 +163,26 @@ extension ObservableArray: MutableCollection {
         let end = elements.count
         elements.append(contentsOf: newElements)
         arrayDidChange(ArrayChangeEvent(inserted: Array(end..<elements.count)))
+        arrayDidChange(ArrayChangeTuple(inserted: newElements.enumerated().map({ ($0, $1) })))
     }
 
     @discardableResult public mutating func removeLast() -> Element {
         let e = elements.removeLast()
         arrayDidChange(ArrayChangeEvent(deleted: [elements.count]))
+        arrayDidChange(ArrayChangeTuple(deleted: [(elements.count, e)]))
         return e
     }
 
     public mutating func insert(_ newElement: Element, at i: Int) {
         elements.insert(newElement, at: i)
         arrayDidChange(ArrayChangeEvent(inserted: [i]))
+        arrayDidChange(ArrayChangeTuple(inserted: [(i, newElement)]))
     }
 
     @discardableResult public mutating func remove(at index: Int) -> Element {
         let e = elements.remove(at: index)
         arrayDidChange(ArrayChangeEvent(deleted: [index]))
+        arrayDidChange(ArrayChangeTuple(deleted: [(index, e)]))
         return e
     }
 
@@ -150,6 +193,7 @@ extension ObservableArray: MutableCollection {
         let es = elements
         elements.removeAll(keepingCapacity: keepCapacity)
         arrayDidChange(ArrayChangeEvent(deleted: Array(0..<es.count)))
+        arrayDidChange(ArrayChangeTuple(inserted: es.enumerated().map({ ($0, $1) })))
     }
 
     public mutating func insertContentsOf(_ newElements: [Element], atIndex i: Int) {
@@ -158,26 +202,30 @@ extension ObservableArray: MutableCollection {
         }
         elements.insert(contentsOf: newElements, at: i)
         arrayDidChange(ArrayChangeEvent(inserted: Array(i..<i + newElements.count)))
+        arrayDidChange(ArrayChangeTuple(inserted: zip(Array(i..<i + newElements.count), newElements).map({ ($0, $1) })))
     }
 
     public mutating func popLast() -> Element? {
-        let e = elements.popLast()
-        if e != nil {
+        if let e = elements.popLast() {
             arrayDidChange(ArrayChangeEvent(deleted: [elements.count]))
+            arrayDidChange(ArrayChangeTuple(deleted: [(elements.count, e)]))
+            return e
         }
-        return e
+        return nil
     }
 }
 
 extension ObservableArray: RangeReplaceableCollection {
     public mutating func replaceSubrange<C : Collection>(_ subRange: Range<Int>, with newCollection: C) where C.Iterator.Element == Element {
         let oldCount = elements.count
+        let oldElements = elements
         elements.replaceSubrange(subRange, with: newCollection)
         let first = subRange.lowerBound
         let newCount = elements.count
         let end = first + (newCount - oldCount) + subRange.count
-        arrayDidChange(ArrayChangeEvent(inserted: Array(first..<end),
-                                        deleted: Array(subRange.lowerBound..<subRange.upperBound)))
+        arrayDidChange(ArrayChangeEvent(inserted: Array(first..<end), deleted: Array(subRange.lowerBound..<subRange.upperBound)))
+        arrayDidChange(ArrayChangeTuple(inserted: zip(Array(first..<end), newCollection).map({ ($0, $1) }), deleted: zip(Array(subRange.lowerBound..<subRange.upperBound), oldElements).map({ ($0, $1) }))
+        )
     }
 }
 
@@ -203,8 +251,10 @@ extension ObservableArray: Sequence {
             elements[index] = newValue
             if index == elements.count {
                 arrayDidChange(ArrayChangeEvent(inserted: [index]))
+                arrayDidChange(ArrayChangeTuple(inserted: [(index, newValue)]))
             } else {
                 arrayDidChange(ArrayChangeEvent(updated: [index]))
+                arrayDidChange(ArrayChangeTuple(updated: [(index, newValue)]))
             }
         }
     }
@@ -214,10 +264,11 @@ extension ObservableArray: Sequence {
             return elements[bounds]
         }
         set {
+            let oldElements = elements[bounds]
             elements[bounds] = newValue
             let first = bounds.lowerBound
-            arrayDidChange(ArrayChangeEvent(inserted: Array(first..<first + newValue.count),
-                                            deleted: Array(bounds.lowerBound..<bounds.upperBound)))
+            arrayDidChange(ArrayChangeEvent(inserted: Array(first..<first + newValue.count), deleted: Array(bounds.lowerBound..<bounds.upperBound)))
+            arrayDidChange(ArrayChangeTuple(inserted: zip(Array(first..<first + newValue.count), newValue).map({ ($0, $1) }), deleted: zip(Array(bounds.lowerBound..<bounds.upperBound), oldElements).map({ ($0, $1) })))
         }
     }
 }
